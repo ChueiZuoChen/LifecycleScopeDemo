98f49c2ba6a315a0b7d909e99c6b363e275f1665
czchen <louisa0103a@gmail.com>
2020-04-27 16:10:45 +0800

    lifecycleScope.launch

---
+++ app/src/main/java/com/example/lifecyclescopedemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.lifecyclescopedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.example.lifecyclescopedemo.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            Log.i("MyTag", "thread is ${Thread.currentThread().name}")
+//            delay(5000)
+//            progressBar.visibility = View.VISIBLE
+//            delay(10000)
+//            progressBar.visibility = View.GONE
+        }
+    }
+}
